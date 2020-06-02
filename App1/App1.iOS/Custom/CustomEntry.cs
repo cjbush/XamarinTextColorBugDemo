3571e0155d312b4c7bc9c7c7631ffbca6eef4ae7
Chris Bush <cbush@aiconline.com>
2020-06-01 21:27:46 -0500

    Initial commit

---
+++ App1/App1.iOS/Custom/CustomEntry.cs
@@ -0,0 +1,23 @@
+﻿using App1.Custom;
+using App1.iOS.Custom;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryIos))]
+
+namespace App1.iOS.Custom
+{
+    public sealed class CustomEntryIos : EntryRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.TextColor = new UIColor(CoreImage.CIColor.WhiteColor); //This blows up
+            Control.TextColor = UIColor.White; //This works fine
+        }
+
+    }
+}
---
+++ App1/App1/Custom/CustomEntry.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace App1.Custom
+{
+    public sealed class CustomEntry : Entry
+    {
+    }
+}
